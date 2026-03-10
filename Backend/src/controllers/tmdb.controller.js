4dae036c69eca5dd02ea5a9d973f32dfae141325
Samarth0802 <samarthvarshney08022005@gmail.com>
2026-03-10 13:20:52 +0530

    addded few more tmdb apis check in tmdb.routes.js

--- Backend/src/controllers/tmdb.controller.js
+++ Backend/src/controllers/tmdb.controller.js
@@ -27,6 +27,13 @@ async function getMovie(req, res, next) {
   try {
     const { page = 1, query } = req.query;
 
+
+    if(!query){
+    return res.status(400).json({
+      message:"query parameter required"
+    })
+
+    }
     const response = await tmdb.get("/search/movie", {
       params: {
         query,
@@ -37,7 +44,8 @@ async function getMovie(req, res, next) {
     res.status(200).json({
       success: true,
       page: response.data.page,
-      results: response.data.results
+      results: response.data.results,
+      total_pages: response.data.total_pages,
     });
 
   } catch (err) {
@@ -46,4 +54,168 @@ async function getMovie(req, res, next) {
   }
 }
 
-export {getAllMovies,getMovie}
+async function getTrendingMovie(req,res,next) {
+  try {
+    const {page=1} = req.query
+    const response = await tmdb.get('/trending/movie/day',{
+      params:{
+        page
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages,
+    })
+
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err)
+  }
+}
+
+async function  getPopularMovies(req,res,next){
+  try {
+    const { page = 1 } = req.query;
+    const response = await tmdb.get('/movie/popular',{
+      params:{
+        page
+      }
+    })    
+
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages,
+    })
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err)
+  }
+}
+
+async function getMovieDetails(req,res,next){
+  try{
+
+    const {id} = req.params;
+
+    const response = await tmdb.get(`/movie/${id}`);
+    
+    res.status(200).json({
+      success: true,
+      data:response.data,
+    })
+
+  }catch(err){
+    err.status = err.status || 500
+    next(err)
+  }
+}
+
+async function getMovieVideo(req,res,next){
+  try {
+    const { id } = req.params;
+
+    const response = await tmdb.get(`/movie/${id}/videos`);
+
+    res.status(200).json({
+      success:true,
+      data:response.data
+    })
+
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err)
+  }
+}
+
+async function getUpcomingMovie(req,res,next){
+  try {
+    const response = await tmdb.get('/movie/upcoming')
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages,
+    })
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err)
+  }
+}
+
+async function getTvShows(req,res,next) {
+  try {
+    const { page = 1 } = req.query;
+    const response = await tmdb.get('/discover/tv',{
+      params:{page}
+    });
+
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages,
+    });
+
+  } catch (err) {
+    err.status = err.status || 500
+    next(err)
+  }
+}
+
+async function getPerson(req, res, next) {
+  try {
+    const { query, page = 1 } = req.query;
+
+    const response = await tmdb.get("/search/person", {
+      params: {
+        query,
+        page
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages,
+    });
+
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err);
+  }
+}
+
+
+async function searchTvShows(req, res, next) {
+  try {
+    const { query, page = 1 } = req.query;
+    
+    const response = await tmdb.get("/search/tv", {
+      params: {
+        query,
+        page
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      page: response.data.page,
+      results: response.data.results,
+      total_pages: response.data.total_pages
+    });
+
+  } catch (err) {
+    err.status = err.status || 500;
+    next(err);
+  }
+}
+
+
+
+export {getAllMovies,getMovie,getTrendingMovie,getPopularMovies,getMovieDetails,getMovieVideo,getUpcomingMovie,getTvShows,getPerson,searchTvShows }
--- Backend/src/routers/tmdb.routes.js
+++ Backend/src/routers/tmdb.routes.js
@@ -1,12 +1,24 @@
 import express from 'express' 
 import { authMiddleware } from '../middleware/auth.middleware.js';
 
-import {getAllMovies,getMovie} from '../controllers/tmdb.controller.js'
+import {getAllMovies,getMovie, getTrendingMovie,getPopularMovies, 
+    getMovieDetails,getMovieVideo,getUpcomingMovie,getTvShows,getPerson,
+    searchTvShows} from '../controllers/tmdb.controller.js'
 
 const movieRouter = express.Router()
 
 
 movieRouter.get('/getAll',authMiddleware,getAllMovies);
 movieRouter.get('/getmovie',authMiddleware,getMovie);
+movieRouter.get('/trending',authMiddleware,getTrendingMovie);
+movieRouter.get('/popular',authMiddleware,getPopularMovies)
+movieRouter.get('/upcoming',authMiddleware,getUpcomingMovie)
+movieRouter.get('/tvShows',authMiddleware,getTvShows)
+movieRouter.get('/popular/person',authMiddleware,getPerson)
+movieRouter.get('/:id',authMiddleware,getMovieDetails)
+movieRouter.get('/:id/video',authMiddleware,getMovieVideo)
+movieRouter.get('/search/tvShows',authMiddleware,searchTvShows)
+
+
 
 export default movieRouter
